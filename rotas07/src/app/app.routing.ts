d103ef6349a8ab732fabfc96ba3b74e0b0c648ec
Felipe chagas <felipereis9998@aluno.saojudas.br>
2023-09-29 20:17:06 -0300

    criando modulos

--- rotas07/src/app/app.routing.ts
+++ rotas07/src/app/app.routing.ts
@@ -1,5 +1,7 @@
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 import { ModuleWithProviders } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
@@ -7,8 +9,10 @@ import { LoginComponent } from './login/login.component';
 
 const APP_ROUTES: Routes = [
   { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
   { path: '', component: HomeComponent },
 ];
 
-const routing: ModuleWithProviders = RouterModule.forRoot;
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
